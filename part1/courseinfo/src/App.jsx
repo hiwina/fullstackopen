886f1ee821cfa891a80d9afea1e5a0ea5dc8636e
hiwina <hiwina@gmail.com>
2023-11-07 23:28:58 +0200

    Commit to Exercise1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,19 +5,23 @@ const Header = (props) => {
     </>
   )
   }
+const Part = (props) => {
+  return (
+    
+   <p>
+        {props.part} {props.exercises}
+      </p>
+   
+  )
+}
 
-const Content = (props) => {
+const Content = () => {
+  
   return (
     <>
-          <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+       <Part part='Fundamentals of React' exercises={10} />
+       <Part part='Using props to pass data' exercises={7} />
+       <Part part='State of a component'exercises={14} />
     </>
   )
 }
@@ -30,18 +34,13 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application developmnet'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1}
-      part2={part2} exercises2={exercises2} 
-      part3={part3} exercises3={exercises3}/>
+      <Content />
      <Total exercises1={exercises1}exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
